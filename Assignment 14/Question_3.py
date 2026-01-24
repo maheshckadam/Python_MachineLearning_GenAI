e50c10334fb89a25b46706ad9e1fbe4d7547f7ab
Mahesh <kadammaheshc@gmail.com>
2026-01-24 23:39:25 +0530

    Assignment 14 Question 3 and 4

---
+++ Assignment 14/Question_3.py
@@ -0,0 +1,19 @@
+#  Accept two numbers and return maximum number using lambda function.
+
+Greater = lambda Value1, Value2: Value1 if Value1 > Value2 else Value2
+
+def main():
+    No1 = 0
+    No2 = 0
+    Result = 0
+
+    No1 = int(input("Enter First Number: "))
+    No2 = int(input("Enter Second Number: "))
+
+    Result = Greater(No1, No2)
+
+    print(Result, "is greater")
+
+if __name__ == "__main__":
+    main()
+
