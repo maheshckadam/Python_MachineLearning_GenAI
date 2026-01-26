17b8e4fde716ac1d0a22deffa707d36733ee19b0
Mahesh <kadammaheshc@gmail.com>
2026-01-26 15:50:45 +0530

    Assignment 16 Question 2

---
+++ Assignment 16/Question_2.py
@@ -0,0 +1,23 @@
+# Check Input Number is Even or Odd.
+
+def ChkNum(Value1):
+    if (Value1 % 2 == 0):
+        return True
+    else:
+        return False
+
+def main():
+    No1 = 0
+    Ans = False
+    
+    No1 = int(input("Enter Number: "))
+    Ans = ChkNum(No1)
+
+    if (Ans == True):
+        print("Even Number")
+    else:
+        print("Odd Number")
+
+
+if __name__ == "__main__":
+    main()
